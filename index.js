2dc0bb5f3f57ce9dbfb11e1f2f122e85b03cdfd9
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-15 20:07:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   const newName = (...driver);
 
-  newName['name'] = 'Bob';
+  newName[name] = 'Bob';
 
   return newName
 }
