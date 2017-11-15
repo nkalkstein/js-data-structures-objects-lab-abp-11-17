c32aed126c25637ac25b23a363da3e71fd2cd181
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-15 19:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ driver = {
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newDriver = {...driver}
 
-  newDriver[name] = 'Bob';
+  newDriver[address] = '11 Broadway';
 
   return newDriver
 }
